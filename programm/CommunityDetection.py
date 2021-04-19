b5d54243852c2de84a1f0b10f75ca218450f7653
Eliseo <eliseowzy@hotmail.com>
2021-04-19 19:51:00 +0800

    add feature: unweighted graph filter, filter the graph according to min degree add feature: unweighted graph file output: output gexf and .gml format graph file for visualization

--- programm/CommunityDetection.py
+++ programm/CommunityDetection.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
-# import community as community_louvain
+import community as community_louvain
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 import networkx as nx
 
 
@@ -46,6 +48,7 @@ def build_email_graph():
         # Load the email graph as csv
         email_graph.to_csv(output_path + i, index=False)
         print("{} success".format(i))
+    return None
 
 
 def clean_graph_data():
@@ -68,35 +71,88 @@ def clean_graph_data():
             new_email = {"From": from_address, "To": to_address}
             email_graph = email_graph.append(new_email, ignore_index=True)
         email_graph.to_csv(output_path + i, index=False)
+    return None
 
 
-def detect_community_unweighted():
+def filter_graph_nodes(min_degree):
+    input_path = r'./data/email_graph/unweighted_clean_data/'
+    G = nx.Graph()
+    for i in os.listdir(input_path):
+        print(i)
+        graph_df = pd.read_csv(input_path + i)
+        for edge in graph_df.itertuples():
+            if edge[1] != edge[2]:
+                start = str(edge[1])
+                end = str(edge[2])
+                if '.' in start:
+                    index_1 = start.index('@')
+                    start = start[0:index_1]
+                if '.' in end:
+                    index_2 = end.index('@')
+                    end = end[0:index_2]
+                G.add_edge(start, end)
+
+    nodes_removed = []
+    for node in G.nodes:
+        if G.degree(node) < min_degree:
+            nodes_removed.append(node)
+    G.remove_nodes_from(nodes_removed)
+    nodes_zero_degree = []
+    for node in G.nodes:
+        if G.degree(node) == 0:
+            nodes_zero_degree.append(node)
+    G.remove_nodes_from(nodes_zero_degree)
+    return G
+
+
+def detect_community_unweighted(min_degree=120):
     """
     Detect communities from unweighted graph
     Returns:
 
     """
-    input_path = r'./data/email_graph/unweighted_clean_data/'
-    G = nx.Graph()
-    for i in os.listdir(input_path):
-        graph_df = pd.read_csv(input_path + i)
-        for edge in graph_df.itertuples():
-            G.add_edge(edge[1], edge[2])
-        # G = nx.from_pandas_dataframe(graph_df)
-        for edge in G.edges:
-            if edge[0] == edge[1]:
-                G.remove_edge(edge[0], edge[1])
-        break
-    print(G.edges)
+    G = filter_graph_nodes(min_degree)
+    # compute the best partition
+    partition = community_louvain.best_partition(G)
+    draw_communities(G, partition)
+    return None
 
-    # G = nx.erdos_renyi_graph(100, 0.01)
-    # partition = community_louvain.best_partition(G)
+
+def output_gexf_file(graph_type, min_degree):
+    G = filter_graph_nodes(min_degree=min_degree)
+    nx.write_gexf(G, r'.\data\email_communities\visualization\gexf_files\{}_min_degree_{}.gexf'.format(graph_type,
+                                                                                                       min_degree))
+    return None
+
+
+def output_gml_file(graph_type, min_degree):
+    G = filter_graph_nodes(min_degree=min_degree)
+    nx.write_gml(G, r'.\data\email_communities\visualization\gml_files\{}_min_degree_{}.gml'.format(graph_type,
+                                                                                                    min_degree))
+    return None
+
+
+def draw_communities(G, partition):
+    # draw the graph
+    pos = nx.spring_layout(G)
+    # color the nodes according to their partition
+    cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+    nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
+                           cmap=cmap, node_color=list(partition.values()))
+    nx.draw_networkx_edges(G, pos, alpha=0.5)
+    # nx.draw_networkx_labels(G, pos)
+    plt.show()
+    # print(G.edges)
+    return None
 
 
 def main():
     # clean_graph_data()
     # build_email_graph()
-    detect_community_unweighted()
+
+    # detect_community_unweighted(min_degree=100)
+    # output_gexf_file(graph_type="unweighted",min_degree=100)
+    output_gml_file(graph_type="unweighted", min_degree=100)
 
 
 if __name__ == '__main__':
--- programm/Test.py
+++ programm/Test.py
@@ -5,4 +5,5 @@ Date: 04-11-2021
 Version: 1.0
 """
 
-print("From	Message_ID	Text	Time_Stamp	To	X_Folder	X_Origin	Subject	Cc".split())
+string = "phillip.allen@enron.com"
+print(string.index('@'))
